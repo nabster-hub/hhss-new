1b3e8b9ff98d71f2332d9e15d180aa1b86508782
Naimanshalov Ilias <mail@nabster.name>
2026-01-13 19:21:54 +0500

    fix

--- src/components/modalTrigger.jsx
+++ src/components/modalTrigger.jsx
@@ -7,7 +7,7 @@ const ModalTrigger = ({mobile}) => {
     return (
        <>
            <button
-                 className={mobile ? "py-5 px-6 text-center font-medium text-xs block w-full bg-brand hover:bg-brand-yellow text-brand-yellow hover:text-brand transition-all rounded-2xl" : "py-5 px-6 bg-brand max-lg:text-center max-lg:font-medium max-lg:text-xs max-lg:w-full hover:bg-brand-yellow text-brand-yellow hover:text-brand transition-all rounded-2xl"}
+                 className={mobile ? "py-5 px-6 text-center font-medium text-xs block w-full bg-brand hover:bg-brand-yellow text-brand-yellow hover:text-brand transition-all rounded-2xl" : "py-5 px-6 bg-brand max-lg:text-center max-lg:font-medium max-lg:text-xs max-lg:w-full hover:bg-brand-yellow text-brand-yellow hover:text-brand transition-all rounded-2xl cursor-pointer"}
                  onClick={()=>setOpen(true)}>CONNECT</button>
 
            {open && ( <Modal onClose={()=>setOpen(false)} />)}
